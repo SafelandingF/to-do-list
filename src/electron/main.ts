4788a8e735a15bd020dda6ca25c9ecc28f2e7de8
fanzixuan <fanzixuan@xiaomi.com>
2025-02-05 17:15:56 +0800

    feat: 完成了基本的配置，可以在本地运行了

---
+++ src/electron/main.ts
@@ -0,0 +1,11 @@
+import { app, BrowserWindow } from 'electron';
+import { isDev } from './utils/env.js';
+
+app.on('ready', () => {
+  const mainWindow = new BrowserWindow();
+  if (isDev()) {
+    mainWindow.loadURL('http://localhost:5173');
+  } else {
+    mainWindow.loadFile('./dist/index.html');
+  }
+});
