0793aad1f02bf51bdb0f10c20cfdd21d5c18bc41
fanzixuan <fanzixuan@xiaomi.com>
2025-02-26 10:32:13 +0800

    feat: 添加handler的hooks

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -5,41 +5,13 @@ import { HtmlHTMLAttributes, useEffect, useRef, useState } from 'react';
 import dayjs from 'dayjs';
 import './App.css';
 import useGlobalConfig from './hooks/useGlobalConfig';
+import useGetComponentSize from './hooks/useGetComponentSize';
 
 function App() {
-  // const [nowDate, setNowData] = useState(dayjs());
-
-  // // 不加useEffect会导致定时器越来越多
-  // useEffect(() => {
-  //   const timeInterval = setInterval(() => {
-  //     setNowData(dayjs());
-  //   }, 1000);
-  //   return () => {
-  //     clearInterval(timeInterval);
-  //   };
-  // }, []);
-
-  // useEffect(() => {
-  //   console.log('nowDate', nowDate);
-  // }, [nowDate]);
+  const [element, setElement] = useState(['name1']);
+  const containerRef = useRef<HTMLDivElement>(null);
   const globalConfig = useGlobalConfig();
-
-  const handleMax = () => {
-    // window.electron.sendFrameAction('maximize');
-    window.electron.sendFrameAction('maximize');
-    console.log('handleMax');
-    console.log(globalConfig.htmlFontSizeWithPx());
-  };
-  // const handleMin = () => {
-  //   window.electron.sendFrameAction('minimize');
-  // };
-  // const handleClose = () => {
-  //   window.electron.sendFrameAction('close');
-  // };
-  const handleSetWindowSize = (payload: FrameWindowSize) => {
-    window.electron.setWindowSize(payload);
-    console.log('handleSetWindowSize', payload);
-  };
+  const mainWinodSize = useGetComponentSize(containerRef);
 
   useGlobalShortcuts(
     [
@@ -50,22 +22,24 @@ function App() {
     ],
     undefined
   );
-  const containerRef = useRef<HTMLDivElement>(null);
 
   return (
     <>
-      <div
-        className="bg-sky-600 box-border  w-[400px] h-30  rounded-3xl"
-        ref={containerRef}
-      >
+      <div className="bg-sky-600 box-border rounded-3xl" ref={containerRef}>
         <p className="handle-drag">这是一段话</p>
         <button
           type="button"
           title="max"
           className=" w-10 h-10 bg-red-200"
-          onClick={() =>
-            console.log(containerRef.current?.getBoundingClientRect())
-          }
+          onClick={() => console.log(mainWinodSize?.width)}
+        >
+          max
+        </button>
+        <button
+          type="button"
+          title="max"
+          className=" w-10 h-10 bg-red-200"
+          onClick={() => console.log(mainWinodSize?.height)}
         >
           max
         </button>
@@ -73,10 +47,13 @@ function App() {
           type="button"
           title="max"
           className=" w-10 h-10 bg-red-200 ml-5"
-          onClick={() => handleSetWindowSize({ width: 500, height: 500 })}
+          onClick={() => setElement(element.concat('name2'))}
         >
           set
         </button>
+        {element.map((item) => {
+          return <div key={item}>{item}</div>;
+        })}
       </div>
     </>
   );
---
+++ src/ui/hooks/useElectronEventHandler.ts
@@ -0,0 +1,24 @@
+const useElectronEventHandler = () => {
+  const handleMax = () => {
+    // window.electron.sendFrameAction('maximize');
+    window.electron.sendFrameAction('maximize');
+  };
+  const handleMin = () => {
+    window.electron.sendFrameAction('minimize');
+  };
+  const handleClose = () => {
+    window.electron.sendFrameAction('close');
+  };
+  const handleSetWindowSize = (payload: FrameWindowSize) => {
+    window.electron.setWindowSize(payload);
+    console.log('handleSetWindowSize', payload);
+  };
+  return {
+    handleMax,
+    handleMin,
+    handleClose,
+    handleSetWindowSize
+  };
+};
+
+export default useElectronEventHandler;
